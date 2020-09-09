fe5872f63eec78341fa3df362e200900e39eca2e
Marc Wagner <marcwagn@zedat.fu-berlin.de>
2020-09-09 19:19:50 +0200

    add image_seg

---
+++ cnn_detection/image_segmentation_keras/keras_segmentation/metrics.py
@@ -0,0 +1,34 @@
+import numpy as np
+from keras import backend as K
+import tensorflow as tf
+
+EPS = 1e-12
+
+#https://github.com/Golbstein/KerasExtras/blob/master/keras_functions.py
+def mean_iou(y_true, y_pred):
+    nb_classes = K.int_shape(y_pred)[-1]
+    iou = []
+    true_pixels = K.argmax(y_true, axis=-1)
+    pred_pixels = K.argmax(y_pred, axis=-1)
+    #void_labels = K.equal(K.sum(y_true, axis=-1), 0)
+    for i in range(0, nb_classes): # exclude first label (background) and last label (void)
+        true_labels = K.equal(true_pixels, i)
+        pred_labels = K.equal(pred_pixels, i)
+        inter = tf.to_int32(true_labels & pred_labels)
+        union = tf.to_int32(true_labels | pred_labels)
+        legal_batches = K.sum(tf.to_int32(true_labels), axis=1)>0
+        ious = K.sum(inter, axis=1)/K.sum(union, axis=1)
+        iou.append(K.mean(tf.gather(ious, indices=tf.where(legal_batches)))) # returns average IoU of the same objects
+    iou = tf.stack(iou)
+    legal_labels = ~tf.debugging.is_nan(iou)
+    iou = tf.gather(iou, indices=tf.where(legal_labels))
+    return K.mean(iou)
+
+def get_iou(gt, pr, n_classes):
+    class_wise = np.zeros(n_classes)
+    for cl in range(n_classes):
+        intersection = np.sum((gt == cl)*(pr == cl))
+        union = np.sum(np.maximum((gt == cl), (pr == cl)))
+        iou = float(intersection)/(union + EPS)
+        class_wise[cl] = iou
+    return class_wise
